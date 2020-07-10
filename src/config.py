ee664c4b13dcc5e5352f7d6234e71e425f9441f2
Ludde <ludde.llj@gmail.com>
2020-07-10 13:28:37 +0200

    config file

---
+++ src/config.py
@@ -0,0 +1,7 @@
+TOKEN = 'ubidots-token-here'
+
+WIFI_SSID = 'wifi-name-here
+WIFI_PASS = 'wifi-password-here'
+
+DELAY      = 600 # seconds : sample frequency
+TRAY_SPACE = 131 # mm : distance between empty tray and distance sensor
